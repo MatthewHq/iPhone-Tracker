fc5d742437f294bd0a4624944830ffe51203a36b
MatthewHoque <hoque.matt@gmail.com>
2021-11-10 16:44:15 -0800

    more details on prototype

--- intialTesting.py
+++ intialTesting.py
@@ -1,29 +1,43 @@
 import json
 
+def check(entry):
+    try:
+        ret=entry
+    except:
+        ret=None
+    return ret
+
 def main():
     data=json.load(open('requestExample.json',))
 
     #check if response has stores
-    try:
-        stores=data["body"]["PickupMessage"]["stores"]
-    except:
-        stores=None
+    stores=check(data["body"]["PickupMessage"]["stores"])
 
     # print(stores)
-    print(data["body"]==None)
-    print(data["body"]["PickupMessage"]==None)
-    print(data["body"]["PickupMessage"]["stores"]==None)
+    # print(data["body"]==None)
+    # print(data["body"]["PickupMessage"]==None)
+    # print(data["body"]["PickupMessage"]["stores"]==None)
 
     if(stores != None):
         for store in range(len(stores)):
             #check if response has parts available
-            try:
-                parts=stores[store]["partsAvailability"]
-            except:
-                parts=None
+            parts=check(stores[store]["partsAvailability"])
             if(parts != None):
-                for key in parts:
-                    print("Key: ",key)
+                for part in parts:
+                    print("==============================")
+                    print(parts[part]["pickupDisplay"]," - ",(str)(parts[part]["storePickupProductTitle"].replace("Â","")))
+                    #check if response has address available
+                    address=check(stores[store]["address"])
+                    if(address != None):
+                        fullAddress=""
+                        for line in address:
+                            if address[line]!= None:
+                                fullAddress+= address[line]+" "
+                    print(fullAddress)
+                
+
+            
+                    
                 
             
 
